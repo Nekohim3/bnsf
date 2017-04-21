60374982160c47f09e2e000b656ef721efe30bc7
Nekhime <Nekhime@NEKHIME->
2017-04-21 21:50:21 +0300

    qwe

--- bnsf/bnsf/Program.cs
+++ bnsf/bnsf/Program.cs
@@ -1,22 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
 
 namespace bnsf
 {
     static class Program
     {
+        private static string path = @"C:\Program Files (x86)\NCSOFT\BnS\contents\Local\NCWEST\data\";
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var th = new Thread(Thf);
+            th.Start();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+        private static void Thf()
+        {
+            var pp = Process.GetProcessesByName("Client").Length;
+            while (true)
+            {
+                var p = Process.GetProcessesByName("Client").Length;
+                if (p == 1)
+                {
+                    if (pp == 0)
+                        File.Copy($"{path}editing\\xml64m.dat", $"{path}xml64.dat", true);
+                }
+                else
+                {
+                    if (pp == 1)
+                        File.Copy($"{path}editing\\xml64.dat", $"{path}xml64.dat", true);
+                }
+                pp = p;
+                Thread.Sleep(1000);
+            }
+
         }
     }
 }
